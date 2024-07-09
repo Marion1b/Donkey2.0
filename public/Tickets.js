3831691aa5e7be7ccf643111fa79bbeff71f85a4
Marion1b <marion.barthoux@gmail.com>
2024-07-09 13:51:27 +0200

    le prix s'affiche pour les tarifs plein et réduits

--- public/Tickets.js
+++ public/Tickets.js
@@ -16,10 +16,48 @@ export class Tickets{
         let totalTickets = 0;
         let totalPay = 0;
         let number;
+        let type;
+        let price=0;
         for(let i in this.days){
             if(this.days[i].value){
+                type = this.days[i].id;
                 number = parseInt(this.days[i].value);
-                total += number;
+                totalTickets = number;
+                switch(type){
+                    case 'friday':
+                        price = 24;
+                        break;
+                    case 'saturday':
+                    case 'sunday':
+                    case 'friday-sunday-reduce':
+                    case 'friday-saturday-reduce':
+                        price = 32;
+                        break;
+                    case 'friday-saturday':
+                    case 'friday-sunday':
+                    case 'all-days-reduce':
+                        price = 50;
+                        break;
+                    case 'saturday-sunday':
+                        price = 58;
+                        break;
+                    case 'all-days':
+                        price = 80;
+                        break;
+                    case 'friday-reduce':
+                        price = 14;
+                        break;
+                    case 'saturday-reduce':
+                    case 'sunday-reduce':
+                        price = 22;
+                        break;
+                    case 'saturday-sunday-reduce':
+                        price = 40;
+                        break;
+                    default:
+                        price = 0;
+                }
+                totalPay += price * totalTickets;
             }
         }
         return totalPay;
--- public/script.js
+++ public/script.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function(){
     const buyTickets = document.querySelector(".buy-tickets");
     if(buyTickets){
         const select = document.querySelectorAll(".buy-tickets select");
+        const par = document.querySelector(".tickets-total p");
         const tickets = new Tickets(select);
         select.forEach((element) =>{
             element.addEventListener('change', (e) =>{
-                // console.log(element);
-                console.log(tickets.total());
+                par.innerText = tickets.total() + "€";
             })
         })
     }

0ca802f995223634d900b06c3ab78d67b9e95601
Marion1b <121030423+Marion1b@users.noreply.github.com>
2024-07-12 10:54:28 +0200

    feat : personnal artist list (#6)

--- managers/ArtistManager.php
+++ managers/ArtistManager.php
@@ -69,4 +69,38 @@ class ArtistManager extends AbstractManager{
         }
         
     }
+
+    public function showFavoriteArtists():? array{
+        $query=$this->db->prepare(
+            "SELECT 
+                artists.id,
+                artists.name,
+                artists.picture,
+                artists.date,
+                artists.biography,
+                artists.playlist
+            FROM artists
+            JOIN artists_users ON artists.id = artists_users.artist_id
+            WHERE artists_users.user_id = :user_id
+            ");
+        $parameters = [
+            'user_id' => $_SESSION["user"]->getId()
+        ];
+        $query->execute($parameters);
+        if($query->rowCount()>=1){
+            $artists = $query->fetchAll(PDO::FETCH_ASSOC);
+            $artistsClass = [];
+            foreach($artists as $artist){
+                $dateString = $artist["date"];
+                $date = DateTime::createFromFormat('Y-m-d H:i:s', $dateString);
+                $artistObj = new Artist($artist["name"], $artist["picture"], $date, $artist["biography"], $artist["playlist"]);
+                $artistObj->setId($artist["id"]);
+                $artistsClass[]=$artistObj;
+            }
+            return $artistsClass;
+        }else{
+            return null;
+        }
+        
+    }
 }
